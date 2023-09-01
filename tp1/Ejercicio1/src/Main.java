aba1b2daeab2d3802504088d6cb3d73f61ff29ae
Santiago Arias <santi99oca@gmail.com>
2023-09-01 14:26:50 -0300

    refactor

--- tp1/Ejercicio1/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.text.ParseException;
-
-public class Main {
-    public static void main(String[] args) throws ParseException {
-        Persona persona = new Persona(95893713);
-        System.out.println(persona.mostraInformacion());
-    }
-}
--- tp1/Ejercicio3/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Plataforma plataforma = new Plataforma();
-    }
-}
