fed418d57cba3ce7ac8ad8c523ff84b9f25133be
Santiago Arias <santi99oca@gmail.com>
2023-09-01 15:56:23 -0300

    refactor

--- tp3/ejercicio6/src/productosCongelados/ProductoCongeladoAire.java
+++ tp3/ejercicio6/src/productosCongelados/ProductoCongeladoAire.java
@@ -1,13 +1,13 @@
 package productosCongelados;
 import productos.ProductoCongelado;
 
-public class ProductocongeladoAire extends ProductoCongelado{
+public class ProductoCongeladoAire extends ProductoCongelado{
     private float nitrogeno;
     private float oxigeno;
     private float co2;
     private float vaporAgua;
 
-    public ProductocongeladoAire(String nombre, String fechaVencimiento, int numeroLote, String fechaEnvasado, String granjaOrigen, int organismoSupervision, float temperaturaRecomendada, float nitrogeno, float oxigeno, float co2, float vaporAgua) {
+    public ProductoCongeladoAire(String nombre, String fechaVencimiento, int numeroLote, String fechaEnvasado, String granjaOrigen, int organismoSupervision, float temperaturaRecomendada, float nitrogeno, float oxigeno, float co2, float vaporAgua) {
         super(nombre, fechaVencimiento, numeroLote, fechaEnvasado, granjaOrigen, organismoSupervision, temperaturaRecomendada);
         this.nitrogeno = nitrogeno;
         this.oxigeno = oxigeno;
